337a0bc734747e0ab054829386c779906bd0b734
dewenni <kontakt@toensjost.de>
2024-04-07 21:51:08 +0200

    bump version

--- include/config.h
+++ include/config.h
@@ -4,7 +4,7 @@
 /*-------------------------------------------------------------------------------
 General Configuration
 --------------------------------------------------------------------------------*/
-#define VERSION "v4.0.0" // internal program version
+#define VERSION "v4.0.1" // internal program version
 
 #define WIFI_RECONNECT 10000 // Delay between wifi reconnection tries
 #define MQTT_RECONNECT 10000 // Delay between mqtt reconnection tries
